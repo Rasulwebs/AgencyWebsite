c333301f44c030efad188a744825402922048d23
Rasulwebs <boboqulovrasul80@gmail.com>
2023-04-16 23:42:22 +0500

    edited

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import "./button.scss"
-const Button = ({txt}) => {
-    return (
-        <>
-            <NavLink to="/" className="heroBtnn">{txt} </NavLink>
-        </>
-    );
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./button.scss";
+const Button = ({ txt }) => {
+  return (
+    <>
+      <NavLink to="/" className="heroBtnn">
+        {txt}{" "}
+      </NavLink>
+    </>
+  );
 };
 
 export default Button;
